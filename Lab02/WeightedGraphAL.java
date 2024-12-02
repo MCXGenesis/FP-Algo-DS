442d786cbe97d4872fa358f9ede160c48acd725c
naufal_maula_ <61588358+MCXGenesis@users.noreply.github.com>
2024-12-02 19:58:47 +0700

    Temporary (LAB02)

---
+++ Lab02/WeightedGraphAL.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class WeightedGraphAL {
+    Map<String, List<Edge>> adjacencyList;
+
+    WeightedGraphAL(){}
+
+    void addEdge(String node1, String node2, int weight){}
+
+    djikstra(String start, String end){}
+
+    @Override
+    String toString(){
+        return null;
+    }
+}
