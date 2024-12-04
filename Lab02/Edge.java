5ad1bdc251e2ff6a93b8139b599b63d13487f879
naufal_maula_ <61588358+MCXGenesis@users.noreply.github.com>
2024-12-04 10:10:04 +0700

    Delete Lab02 directory

--- Lab02/Edge.java
+++
@@ -1,9 +0,0 @@
-class Edge {
-    private String targetNode;
-    private int weight;
-
-    Edge(String targetNode, int weight){
-        this.targetNode = targetNode;
-        this.weight = weight;
-    }
-}
--- Lab02/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    private String node;
-     private int distance;
-
-     Node(String node, int distance){
-        this.node = node;
-        this.distance = distance;
-     }
-}
