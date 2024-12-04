513612e9df85d1f8dd52c60480c6e7c5616a95f2
naufal_maula_ <61588358+MCXGenesis@users.noreply.github.com>
2024-12-04 10:08:51 +0700

    add tictactoe
    create tictactoe (incomplete)

---
+++ tictactoe/Main.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Tic Tac Toe ");
+
+        TicTacToe game = new TicTacToe();
+        game.newGame();
+    }
+}
