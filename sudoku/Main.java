1a0b9ac4f6d8a5f049e5e97ccad9ee557a4ee37e
AilaChayra <lailatulfika519@gmail.com>
2024-12-06 16:39:42 +0700

    Identitas Anggota Kelompok

--- sudoku/Main.java
+++ sudoku/Main.java
@@ -1,3 +1,10 @@
+/*
+Algoritma Struktur Data
+Kelas   : C
+Anggota :   Naufal Maula Nabil  - 5026231107
+            Annisa Nur Fauzi    - 5026231228
+            Lailatul Fitaliqoh  - 5026231229
+ */
 package sudoku;
 public class Main {
     public static void main(String[] args) {
