3815a28e181cfaf0ef809dee2e0671bcefccc47d
AilaChayra <lailatulfika519@gmail.com>
2024-12-21 12:50:49 +0700

    Nama Kelompok

--- sudoku/Main.java
+++ sudoku/Main.java
@@ -1,9 +1,11 @@
-/*
-Algoritma Struktur Data
-Kelas   : C
-Anggota :   Naufal Maula Nabil  - 5026231107
-            Annisa Nur Fauzi    - 5026231228
-            Lailatul Fitaliqoh  - 5026231229
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #3
+ * 1 - 5026231107 - Naufal Maula Nabil
+ * 2 - 5026231228 - Annisa Nur Fauzi
+ * 3 - 5026231229 - Lailatul Fitaliqoh
  */
 package sudoku;
 
--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #3
+ * 1 - 5026231107 - Naufal Maula Nabil
+ * 2 - 5026231228 - Annisa Nur Fauzi
+ * 3 - 5026231229 - Lailatul Fitaliqoh
+ */
 package tictactoe;
 
 import java.awt.*;
